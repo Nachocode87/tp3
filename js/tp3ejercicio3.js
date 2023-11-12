0487585bd7ffe97ea7d756c27696699370f8beaf
nachocode <lic.medinajimenez@gmail.com>
2023-11-12 20:18:58 -0300

    agregue ultimo ejercicio

--- js/tp3ejercicio3.js
+++ js/tp3ejercicio3.js
@@ -1,31 +1,36 @@
-let ciudad = [];
 
-do {
-    ciudad.push(window.prompt("escriba ciudades"));
-    
-} while (confirm('desea continuar'));
 
-document.write(`<ul>`);
-document.write(`<li>el arreglo de ciudades tiene ${ciudad.length} elementos</li>`);
-document.write(`<li>el elemento primero es ${ciudad[0]}</li>`);
-document.write(`<li>el elemento tercero es ${ciudad[2]}</li>`);
-document.write(`<li>el elemento ultimo es ${ciudad[ciudad.length-1]}</li>`);
-ciudad.splice(ciudad.length,1,'Paris')
-document.write(`<li>el elemento ultimo es ${ciudad[ciudad.length-1]}</li>`);
-document.write(`</ul>`);
-
-document.write(`Arreglo de ciudades`);
-document.write(`<ul>`);
-for (let i = 0; i < ciudad.length; i++) {
-    
-    
-    document.write(`<li>${ ciudad[i]}  </li>`);       
-    
+const random = function() {
+    let dado = Math.floor(Math.random()*(7 - 1)+1);
+    let dado2 = Math.floor(Math.random()*(7 - 1)+1);
+    return dado + dado2
 }
-document.write(`</ul>`);
 
-document.write(`<ul>`);
-document.write(`<li>el elemento segundo es ${ciudad[1]}</li>`);
-ciudad.splice(1,1,'Barcelona')
-document.write(`<li>el elemento segundo sustituido es ${ciudad[1]}</li>`);
-document.write(`</ul>`);
+let apariciones = [0,0,0,0,0,0,0,0,0,0,0,0,0];
+// document.write(!apariciones[3])
+for (let i = 0; i < 50; i++) {
+    let num = random()
+    // document.write('<p> renerado'+num+'</p>')
+    if (apariciones[num]=== 0) {
+        apariciones[num] = 1        
+        // document.write('<p> no esta'+apariciones[num]+'</p>')
+    } else {
+        apariciones[num] += 1 
+        // document.write('<p> si esta'+apariciones[num]+'</p>')
+    }
+}
+
+document.write(`<table>`);
+document.write(`<tr>`);
+document.write(`<th>Suma🎲🎲 </th>`);
+document.write(`<th>Apariciones🎯</th>`);
+document.write(`<tr>`);
+
+for (let i = 2; i < apariciones.length; i++) {
+    document.write(`<tr>`);
+    document.write(`<th>${i}</th>`);
+    document.write(`<th>${apariciones[i]}</th>`); 
+    document.write(`</tr>`);
+}
+
+document.write(`</table>`);
